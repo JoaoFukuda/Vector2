06cce7bbf7a66be59c4f2654deb4d11ab765d012
João Francisco Cocca Fukuda <joao.fukuda@usp.br>
2020-01-30 04:36:50 -0300

    Initial commit
    * Vector2 structure with basic arithmetic functionalities and some
      utilities (stream "operator <<" overload and user-defined type cast)
    * If I (or any of you) add any other functionalities, you'll se other commit. Else, done!

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Vector2
-Simple Vector2 structure for C++
+A simple and adaptive Vector2 structure for C++
+
+It's just a thing I use and thought about sharing with you guys. Nothing fancy, but still useful.
+
+## How to use
+Just include the `vector2.hpp` file and use it like so:
+
+```
+#include "vector2.hpp"
+
+// Do stuff
+
+Vector2<float> position;
+Vector2<int> target;
+```
+
+No need to compile or link it anywhere.
+
---
+++ vector2.hpp
@@ -0,0 +1,98 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 João Francisco Cocca Fukuda
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#pragma once
+
+#include <cmath>
+#include <ostream>
+
+template<class T>
+struct Vector2
+{
+	T x, y;
+
+	Vector2<T> operator+(const Vector2<T> &other) const
+	{
+		return {x + other.x, y + other.y};
+	}
+
+	void operator+=(const Vector2<T> &other)
+	{
+		x += other.x;
+		y += other.y;
+	}
+
+	Vector2<T> operator-(const Vector2<T> &other) const
+	{
+		return {x - other.x, y - other.y};
+	}
+
+	void operator-=(const Vector2<T> &other)
+	{
+		x -= other.x;
+		y -= other.y;
+	}
+
+	Vector2<T> operator*(double value) const
+	{
+		return {static_cast<T>(x * value), static_cast<T>(y * value)};
+	}
+
+	void operator*=(double value)
+	{
+		x *= value;
+		y *= value;
+	}
+
+	Vector2<T> operator/(double value) const
+	{
+		return {static_cast<T>(x / value), static_cast<T>(y / value)};
+	}
+
+	void operator/=(double value)
+	{
+		x /= value;
+		y /= value;
+	}
+
+	template <class U>
+	operator Vector2<U>() const
+	{
+		return {static_cast<U>(x), static_cast<U>(y)};
+	}
+
+	double Distance(const Vector2<T> &other = {0, 0}) const
+	{
+		Vector2<T> difference {other.x - x, other.y - y};
+		return sqrt(((difference.x)*(difference.x))+((difference.y)*(difference.y)));
+	}
+};
+
+template <class T>
+std::ostream &operator<<(std::ostream &stream, const Vector2<T> vector2)
+{
+	stream << "(" << vector2.x << ", " << vector2.y << ")";
+	return stream;
+}
+
